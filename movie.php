2c6410851fc5064b40af86b986095abe7e0c7f0c
seniferraz <inesferra95@gmail.com>
2016-12-18 20:47:37 +0000

    navbar and index stuff

--- movie.php
+++ movie.php
@@ -93,234 +93,180 @@
 
 ?>
 
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
+<head>
 
-        <!-- META TAGS -->
-        <meta charset="UTF-8" />
-        <title>Spotlight</title>
-
-
-        <!-- STYLESHEETS -->
-
-        <link rel="stylesheet" href="assets/css/flexboxgrid.min.css" type="text/css">
-
-        <link rel="stylesheet" href="assets/css/_font-awesome.min.css.scss" type="text/css">
-
-        <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <!-- META TAGS -->
+    <meta charset="UTF-8" />
+    <title>Spotlight</title>
 
 
-    </head>
+    <!-- STYLESHEETS -->
+
+    <link rel="stylesheet" href="assets/css/flexboxgrid.min.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/_font-awesome.min.css.scss" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
 
 
-    <body>
-
-        <!------------#NAVBAR_BIG------------>
-        <div class="smalltext nav-big">
-
-            <nav class="navbar">
-                <div class="row middle-xs full-height">
-
-                    <ul class="smalltext col-xs-8 end-xs  col-sm-10 col-lg-10 text-bold">
-
-                        <li class="navbar__link"><a href="index.php" class="menu-selected">HOME</a></li>
-                        <li class="navbar__link"><a href="tops.php">TOPS</a></li>
-                        <li class="navbar__link"><a href="slist.php">SONG LIST</a></li>
-                        <li class="navbar__link"> <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">CONTACT US</a></li>
-                        <li class="navbar__link"><a href="search.php">SEARCH</a></li>
-
-                    </ul>
-
-                </div>
-            </nav>
-        </div>
+</head>
 
 
-        <!------------#NAVBAR_SMALL------------>
+<body>
 
-        <div class="nav-small text-bold">
+    <?php include 'navbar.php'; ?>
 
-            <input type="checkbox" id="nav-trigger" class="nav-controller" />
-
-            <header class="header-bar">
-                <label class="" for="nav-trigger" tabindex="-1">
-                    <div class="button--icon-container">
-                        <span class="icon icon--hamburger"></span>
-                    </div>
-                </label>
-
-            </header>
-
-            <aside class="nav">
-                <label class="overlay" for="nav-trigger"></label>
-                <div class="nav__body">
+    <section class="section-resized">
 
 
-                    <ul class="  nav__list col-xs-12 subtitle">
-                        <label class="nav__item" for="nav-trigger">
+        <div class="row">
 
-                            <li><a class="nav__link start-xs" href="index.php" class="menu-selected">HOME</a></li>
-                            <li><a class="nav__link start-xs" href="tops.php">TOPS</a></li>
-                            <li><a class="nav__link start-xs" href="slist.php">SONG LIST</a></li>
-                            <li><a class="nav__link start-xs" href="mailto:someone@example.com?Subject=Hello%20again" target="_top">CONTACT US</a></li>
-                            <li><a class="nav__link start-xs" href="search.php">SEARCH</a></li>
+            <div class="title col-xs-12 start-xs">
+                <p>
 
-                        </label>
-                    </ul>
-                </div>
-            </aside>
-        </div>
-
-        <section class="section-resized">
-
-
-            <div class="row">
-
-                <div class="title col-xs-12 start-xs">
-                    <p>
-
-                        <?php 
-
-                        //-----------------------RESULTADOS - NOME DO FILME e IMAGEM-----------------------//
-
-                        if ($result_filme->num_rows > 0) {
-                            while($row = $result_filme->fetch_assoc()) {
-                                
-                                echo $row["filme"] . "</p>
-                                
-                                    </div>
-                                    
-                                        <div class ='col-sm-6 col-xs-12'>
-                                            <a class ='nav__link center-xs menu-selected'>
-                                            <img src = " . $row["image"] . " class ='logo'> </a>
-                                        </div>";
-                            }
-                        }
-                    ?>
-
-                            <div class="col-sm-6 col-xs-12">
-
-                                <p class="text text-left middle-xs">
-
-                                    <?php 
-                    
-                                    //--------------RESULTADOS - INFO FILME e ATORES e GÉNERO----------------//
-
-                                        while($row = $result_filme_more->fetch_assoc()) {
-
-                                            echo "<b>Release date: </b>" . $row["data_lanc"] . "<br>";
-                                            echo "<br><b>Age rating: </b>" . $row["classif"];
-                                            echo "<br><b>Director: </b>" . $row["realizador"];
-                                            echo "<br><b>IMDB rating: </b>" . $row["imdb_rating"] . "/10";
-                                            echo "<br><b>OST rating: </b>" . $row["ost_rating"] . "/100" . "<br><br>";
-                                        }
-                                    
-                                        echo "<b>Main actors: </b>";
-                                        
-                                        while($row = $result_atores->fetch_assoc()) {
-
-                                            echo "<br>" . $row["nome_ator"];
-                                        }
-                                
-                                        echo "<br><br><b>Genres: </b>";
-                                        
-                                        while($row = $result_generos->fetch_assoc()) {
-
-                                            echo "<br>" . $row["nome_genero"];
-                                        }
-                                
-                                    ?>
-                                </p>
-                </div>
-
-            </div>
-
-            <div class="row center-xs start-md">
-                <div class="col-xs-12 col-sm-6 order-xs-1st">
-
-                    <div class="subtitle  center-xs start-sm">
-                        <p>OFFICIAL SOUNDTRACK</p>
-                    </div>
                     <?php 
 
-                    //--------------------------RESULTADOS - MUSICAS--------------------------//
+                    //-----------------------RESULTADOS - NOME DO FILME e IMAGEM-----------------------//
 
-                        while($row = $result_musicas->fetch_assoc()) {
+                    if ($result_filme->num_rows > 0) {
+                        while($row = $result_filme->fetch_assoc()) {
 
-                            echo "<div class='row center-xs start-md'>
-                                    <div class='col-xs-12 col-sm-7'>
-                                    <p class='text text-left middle-xs'><b>Song: </b>" . $row["nome_musica"] . "
-                                    <br><b>Singer/Band: </b>" . $row["cantor"] . "</p>
-                                    </div>
+                            echo $row["filme"] . "</p>
+
+                                </div>
+
+                                    <div class ='col-sm-6 col-xs-12'>
+                                        <a class ='nav__link center-xs menu-selected'>
+                                        <img src = " . $row["image"] . " class ='logo'> </a>
                                     </div>";
                         }
+                    }
+                ?>
 
-                    ?>
+                        <div class="col-sm-6 col-xs-12">
 
+                            <p class="text text-left middle-xs">
+
+                                <?php 
+
+                                //--------------RESULTADOS - INFO FILME e ATORES e GÉNERO----------------//
+
+                                    while($row = $result_filme_more->fetch_assoc()) {
+
+                                        echo "<b>Release date: </b>" . $row["data_lanc"] . "<br>";
+                                        echo "<br><b>Age rating: </b>" . $row["classif"];
+                                        echo "<br><b>Director: </b>" . $row["realizador"];
+                                        echo "<br><b>IMDB rating: </b>" . $row["imdb_rating"] . "/10";
+                                        echo "<br><b>OST rating: </b>" . $row["ost_rating"] . "/100" . "<br><br>";
+                                    }
+
+                                    echo "<b>Main actors: </b>";
+
+                                    while($row = $result_atores->fetch_assoc()) {
+
+                                        echo "<br>" . $row["nome_ator"];
+                                    }
+
+                                    echo "<br><br><b>Genres: </b>";
+
+                                    while($row = $result_generos->fetch_assoc()) {
+
+                                        echo "<br>" . $row["nome_genero"];
+                                    }
+
+                                ?>
+                            </p>
+            </div>
+
+        </div>
+
+        <div class="row center-xs start-md">
+            <div class="col-xs-12 col-sm-6 order-xs-1st">
+
+                <div class="subtitle  center-xs start-sm">
+                    <p>OFFICIAL SOUNDTRACK</p>
                 </div>
+                <?php 
+
+                //--------------------------RESULTADOS - MUSICAS--------------------------//
+
+                    while($row = $result_musicas->fetch_assoc()) {
+
+                        echo "<div class='row center-xs start-md'>
+                                <div class='col-xs-12 col-sm-7'>
+                                <p class='text text-left middle-xs'><b>Song: </b>" . $row["nome_musica"] . "
+                                <br><b>Singer/Band: </b>" . $row["cantor"] . "</p>
+                                </div>
+                                </div>";
+                    }
+
+                ?>
 
             </div>
 
-            <!--------------MODAL---------->
+        </div>
 
-            <div class="row center-xs">
+        <!--------------MODAL---------->
 
-                <button class="btn-default  md-trigger" data-modal="modal-1">HELP US GROW</button>
+        <div class="row center-xs">
 
-                <div class="md-modal-xs md-effect-1" id="modal-1">
-                    <div class="md-content-xs">
-                        <button class="md-close btn-default">Close me!</button>
+            <button class="btn-default  md-trigger" data-modal="modal-1">HELP US GROW</button>
 
-                        <div>
-                            <form method="post">
-                                <label>Song</label>
-                                <input type="text" name="nome_musica">
+            <div class="md-modal-xs md-effect-1" id="modal-1">
+                <div class="md-content-xs">
+                    <button class="md-close btn-default">Close me!</button>
+
+                    <div>
+                        <form method="post">
+                            <label>Song</label>
+                            <input type="text" name="nome_musica">
+                            <br>
+
+                            <label>Genre</label>
+                            <input type="text" name="genero">
+                            <br>
+
+                            <label>Year</label>
+                            <input type="text" pattern="\d*" maxlength="4" name="ano">
+                            <br>
+
+                            <label>Singer/Band</label>
+                            <input type="text" name="cantor">
+                            <br>
+
+                            <!--
+                            <label name="filters" class="input-anim" for="">
+                                <span class="label__info" value="cantor">Singer/Band</span>
+                                <input type="text" name="option">
                                 <br>
-
-                                <label>Genre</label>
-                                <input type="text" name="genero">
-                                <br>
-
-                                <label>Year</label>
-                                <input type="text" pattern="\d*" maxlength="4" name="ano">
-                                <br>
-
-                                <label>Singer/Band</label>
-                                <input type="text" name="cantor">
-                                <br>
-
-                                <!--
-                                <label name="filters" class="input-anim" for="">
-                                    <span class="label__info" value="cantor">Singer/Band</span>
-                                    <input type="text" name="option">
-                                    <br>
-                                </label>
+                            </label>
 -->
-                                <br>
-                                <input type="submit" value="Add song">
-                            </form>
-                        </div>
+                            <br>
+                            <input type="submit" value="Add song">
+                        </form>
                     </div>
                 </div>
-
             </div>
 
-        </section>
+        </div>
 
-        <div class="md-overlay"></div>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-        <script type="text/javascript" src="assets/js/classie.js"></script>
-        <script type="text/javascript" src="assets/js/modalEffects.js"></script>
-        <script src="assets/js/cssParser.js"></script>
+    </section>
 
-        <!-- jQuery -->
-        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <div class="md-overlay"></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
+    <script type="text/javascript" src="assets/js/classie.js"></script>
+    <script type="text/javascript" src="assets/js/modalEffects.js"></script>
+    <script src="assets/js/cssParser.js"></script>
 
-        <script src="assets/js/script-movie.js"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
-    </body>
+    <script src="assets/js/script-movie.js"></script>
+
+</body>
 
 
-    </html>
+</html>
--- tops-imdb.php
+++ tops-imdb.php
@@ -38,76 +38,22 @@
 
     <body>
 
+        <?php include 'navbar.php'; ?>
 
-        <!------------#NAVBAR_BIG------------>
-        <div class="smalltext nav-big">
+            <section class="section-resized">
 
-            <nav class="navbar">
-                <div class="row middle-xs full-height">
+                <div class="row">
 
-                    <ul class="smalltext col-xs-8 end-xs  col-sm-10 col-lg-10 text-bold">
-
-                        <li class="navbar__link"><a href="index.php" class="menu-selected">HOME</a></li>
-                        <li class="navbar__link"><a href="tops.php">TOPS</a></li>
-                        <li class="navbar__link"><a href="slist.php">SONG LIST</a></li>
-                        <li class="navbar__link"> <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">CONTACT US</a></li>
-                        <li class="navbar__link"><a href="search.php">SEARCH</a></li>
-
-                    </ul>
-
-                </div>
-            </nav>
-        </div>
-
-        <!------------#NAVBAR_SMALL------------>
-
-        <div class="nav-small text-bold">
-
-            <input type="checkbox" id="nav-trigger" class="nav-controller" />
-
-            <header class="header-bar">
-                <label class="" for="nav-trigger" tabindex="-1">
-                    <div class="button--icon-container">
-                        <span class="icon icon--hamburger"></span>
+                    <div class="subtitle start-xs">
+                        <p>IMDB RATING TOP 25 MOVIES</p>
                     </div>
-                </label>
-
-            </header>
-
-            <aside class="nav">
-                <label class="overlay" for="nav-trigger"></label>
-                <div class="nav__body">
-
-
-                    <ul class="  nav__list col-xs-12 subtitle">
-                        <label class="nav__item" for="nav-trigger">
-
-                            <li><a class="nav__link start-xs" href="index.php" class="menu-selected">HOME</a></li>
-                            <li><a class="nav__link start-xs" href="tops.php">TOPS</a></li>
-                            <li><a class="nav__link start-xs" href="slist.php">SONG LIST</a></li>
-                            <li><a class="nav__link start-xs" href="mailto:someone@example.com?Subject=Hello%20again" target="_top">CONTACT US</a></li>
-                            <li><a class="nav__link start-xs" href="search.php">SEARCH</a></li>
-
-                        </label>
-                    </ul>
                 </div>
-            </aside>
-        </div>
 
-        <section class="section-resized">
+                <div class="row">
 
-            <div class="row">
+                    <div class="col-xs-12 start-xs">
 
-                <div class="subtitle start-xs">
-                    <p>IMDB RATING TOP 25 MOVIES</p>
-                </div>
-            </div>
-
-            <div class="row">
-
-                <div class="col-xs-12 start-xs">
-
-                    <?php
+                        <?php
 
                         $numrows = 0;
 
@@ -134,17 +80,17 @@
 
                 ?>
 
+                    </div>
+
                 </div>
 
-            </div>
+            </section>
 
-        </section>
-
-        <div class="md-overlay"></div>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-        <script type="text/javascript" src="assets/js/classie.js"></script>
-        <script type="text/javascript" src="assets/js/modalEffects.js"></script>
-        <script src="assets/js/cssParser.js"></script>
+            <div class="md-overlay"></div>
+            <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
+            <script type="text/javascript" src="assets/js/classie.js"></script>
+            <script type="text/javascript" src="assets/js/modalEffects.js"></script>
+            <script src="assets/js/cssParser.js"></script>
 
     </body>
 
