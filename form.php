bccf460a4cd4f4ff476d897a8cd059f6ff6074e8
afvc <anafilipavc@gmail.com>
2016-12-29 16:32:27 +0000

    login + register + login alerts + register alerts + change password

--- form.php
+++ form.php
@@ -1,11 +1,11 @@
 <!---- Acesso à base de bados --->
 <?php 
 
-    include 'login.php'; 
-/*echo $_SESSION['erros'];*/ 
-/*  if( isset($_SESSION['erros'])){
- echo "<script>  alert('Username or Password is invalid')</script>"
-} */ 
+   include 'login.php'; 
+   include 'register.php'; 
+  
+ 
+  
  ?>
     <!DOCTYPE html>
     <html>
@@ -13,30 +13,70 @@
     <head>
         <title>Spotlight Login</title>
         <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+         <link href="assets/css/flexboxgrid.min.css" rel="stylesheet" type="text/css">
         <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
     </head>
 
     <body>
         <div class="bg-black">
             <section class="section-resized">
+                  <div class="row">
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-xs-6">
                         <p class="title text-lightest">Login </p>
-                        <div id="login">
+                        <div class="login">
 
                             <form action="" method="post">
+                               
+                               <?php
+			if ( isset($LerrMSG) ) {
+				
+				?>
+				<div class="">
+            	<div class=" text-bold text-light alert alert-<?php echo ($LerrTyp=="success") ? "success" : $LerrTyp; ?>">
+				<span class=""></span> <?php echo $LerrMSG; ?>
+                </div>
+            	</div>
+                <?php
+			}
+			?>
+                               
                                 <label class="text-light">UserName :</label>
-                                <input id="name" name="user" placeholder="username" type="text">
+                                <input id="name" name="user" placeholder="username" type="text" >
+                                 <span class="text-danger"><br><?php echo $LusernameError; ?><br></span>
+                                 
                                 <label class="text-light">Password :</label>
                                 <input id="password" name="pass" placeholder="**********" type="password">
-                                
-                                <input name="submit" type="submit" class="btn-dark " value=" Login ">
+                                  <span class="text-danger"><br><?php echo $LpassError; ?><br></span>
 
+                                <div class="end-xs"> <input name="submit" type="submit" class="btn-dark " value=" Login ">
+                                </div>               
                             </form>
+                            
+                            
                         </div>
+                      
                     </div>
-            </section>
+          <!--<div class="col-sm-9 col-xs-12 end-xs"> <br><br><a class="subtitle text-lightest" href="register.php">Register</a> </div>-->
             </div>
+            <div class="row col-xs-offset-4">
+                    <div class="col-xs-12">
+                        <p class="title text-lightest">Register </p>
+                        <div class="login">
+
+                       <?php 
+
+   include 'register-form.php'; 
+  
+ ?>     
+                            
+                        </div>
+                      
+                    </div>
+          <!-- <div class=" col-xs-12 col-sm-9 end-xs"> <br><br><a class="subtitle text-lightest" href="register.php">Register</a> </div>--> 
+            </div>
+                </div>
+             </section>
         </div>
     </body>
 
